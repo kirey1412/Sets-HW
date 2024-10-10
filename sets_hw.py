1e69076a7ecd8ae8063cdb68001b1553d9b8ca38
kirey1412 <kireykayana14@gmail.com>
2024-10-10 19:08:36 +0700

    Add files via upload

---
+++ sets_hw.py
@@ -0,0 +1,22 @@
+a=[1,2,3,4,5]
+set=set(a)
+
+print(set)
+
+#Adding element to a set
+set.add(6)
+print(set)
+
+#Removing element from a set
+set.remove(4)
+print(set)
+
+b = {1,2,3,4,5}
+my_set = {1,2,3,4,5}
+my_sets = {6,7,8,9,10}
+#USE CURLY BRACKETS TO MAKE A SET!
+
+#Union
+print(my_set|my_sets)
+print (my_set|b)
+
